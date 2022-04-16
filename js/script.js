47cf2735ebe6d0d6e004d51f2e0e44123d8198af
Mateusz Przybysz <38528098+maprzybysz@users.noreply.github.com>
2022-04-16 18:53:02 +0200

    let and const variables

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-console.log('some text');
-console.log('some text');
+const name = 'Mateusz';
+console.log(name);
+
+let age = 23;
+console.log(age);
+age = 45;
+console.log(age);
+
+let color;
+
+color = 'red';
+
+let myName = 'Mateusz';
+let myAge = 23;
+let myFavoriteDish = 'burger';
+
+console.log(myName);
+console.log(myAge);
+console.log(myFavoriteDish);
