063c439609c5fd1686001bcd415a5910dfa9f3bc
Mateusz Przybysz <38528098+maprzybysz@users.noreply.github.com>
2022-04-16 19:47:54 +0200

    template string

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-//camelCase, English variable names, JS is case sensitive
-const firstName = 'Mateusz';
+const name = 'Mateusz';
+const favMeal = 'burger';
+
+console.log('Cześć, jestem ' + name + ' i moim ulubionym daniem jest ' + favMeal + '.');
+
+console.log(`Cześć, jestem ${name} i moim ulubionym daniem jest ${favMeal}.`);
+
+const myName = 'Mateusz';
+const petName = 'Miśka';
+
+console.log(`Cześć, jesten ${myName}, a to jest ${petName} - mój królik.`);
+
+const age = 2;
+
+console.log(petName + ' ma już ' + age + ' lata.');
