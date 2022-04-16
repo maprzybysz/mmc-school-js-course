d8443e4bca11eeffa4bd17688d4bfa550747a92d
Mateusz Przybysz <38528098+maprzybysz@users.noreply.github.com>
2022-04-16 18:57:14 +0200

    variable var

--- js/script.js
+++ js/script.js
@@ -1,19 +1,2 @@
-const name = 'Mateusz';
-console.log(name);
-
-let age = 23;
-console.log(age);
-age = 45;
-console.log(age);
-
-let color;
-
-color = 'red';
-
-let myName = 'Mateusz';
-let myAge = 23;
-let myFavoriteDish = 'burger';
-
-console.log(myName);
-console.log(myAge);
-console.log(myFavoriteDish);
+// var is deprecated
+var name = 'myName';
