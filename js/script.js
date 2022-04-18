7b34784adc888acc65b20ef296e50c4cbdd4304b
mprzybysz <notentego678@gmail.com>
2022-04-18 11:45:13 +0200

    conditional operator

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-const day = 'sobota';
+const x = 100;
 
-switch (day) {
-	case 'poniedziałek':
-		console.log('Dziś jest poniedziałek');
-		break;
-	case 'wtorek':
-		console.log('Dziś jest wtorek');
-		break;
-	case 'środa':
-		console.log('Dziś jest środa');
-		break;
-	case 'czwartek':
-		console.log('Dziś jest czwartek');
-		break;
-	case 'piątek':
-		console.log('Dziś jest piątek');
-		break;
-	default:
-		console.log('Weekend!');
+if (x > 20) {
+	console.log(`${x} > 20`);
+} else {
+	console.log(`${x} < 20`);
 }
+
+x > 20 ? console.log(`${x} > 20`) : console.log(`${x} < 20`);
+
+const isLoggedIn = true;
+
+function loggedIn() {
+	console.log('Użytkownik jest zalogowany');
+}
+
+function loggedOut() {
+	console.log('Użytkownik nie jest zalogowany');
+}
+
+isLoggedIn ? loggedIn() : loggedOut();
