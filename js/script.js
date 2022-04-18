1755218814f19aa6a72c1665acd9ab819c0b18d0
mprzybysz <notentego678@gmail.com>
2022-04-18 14:53:23 +0200

    DOM, window, document

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
-let num = 10;
-let numbers = [];
+console.log(window);
 
-for (let i = 0; i < num; i++) {
-	numbers.push(i);
-}
+window.console.log('hello!');
 
-const fn = x => {
-	if (x % 3 === 0 && x !== 0) {
-		console.log(`${x} jest podzielne przez 3`);
-	} else {
-		console.log(`${x} nie jest podzielne przez 3 lub x = 0`);
-	}
-};
+console.log(document);
 
-numbers.forEach(fn);
+console.log()
