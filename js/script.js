6b923751cda0d1152d63bc7d29c1b806f9ab2c5d
mprzybysz <notentego678@gmail.com>
2022-04-18 11:55:30 +0200

    task 5

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
-const promo = '20%';
+const x = 11;
 
-switch (promo) {
-	case '10%':
-		console.log('Dziś mamy 10% zniżki!');
-		break;
-	case '20%':
-		console.log('Dziś mamy 20% zniżki!');
-		break;
-	case '30%':
-		console.log('Dziś mamy 30% zniżki!');
-		break;
-	default:
-		console.log(`Dziś mamy ${promo} zniżki!`);
-		break;
-}
+x % 2 === 0 ? console.log(`Liczba ${x} jest parzysta`) : console.log(`Liczba ${x} nie jest parzysta`);
