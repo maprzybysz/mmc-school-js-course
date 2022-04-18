ecbfaaa84dc277c0de0afdad91082b833f4e4937
mprzybysz <notentego678@gmail.com>
2022-04-18 11:33:55 +0200

    conditional statements

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
-// && - and
-// || - or
-// ! - negation
+const pass = '2usdsadasd232sda!';
 
-if (true && !false) {
-	console.log('true');
+// if (pass.length > 10) {
+// 	console.log('Masz dobre hasło');
+// } else {
+// 	console.log('Masz za krótkie hasło');
+// }
+
+// const color = 'blue';
+
+// if (color == 'blue') {
+// 	if (10 > 0) {
+// 		console.log('true');
+// 	} else {
+// 		console.log('false');
+// 	}
+// } else {
+// 	console.log('fasle');
+// }
+
+if (pass.length > 10 && pass.includes('!')) {
+	console.log('Masz rewelacyjne hasło');
+} else if (pass.length > 10) {
+	console.log('Masz dobre hasło');
 } else {
-	console.log('false');
+	console.log('Masz za krótkie hasło');
 }
