8315244e4301696a2e00a503b3871230fdabb567
mprzybysz <notentego678@gmail.com>
2022-04-18 13:16:12 +0200

    map vs forEach

--- js/script.js
+++ js/script.js
@@ -1,12 +1,6 @@
 const numbers = [0, 10, 23, 100, 252, 258, 2000];
 
-function isEven(x) {
-	return x % 2 === 0;
-}
-
-console.log(numbers.filter(isEven));
-
-numbers.forEach(number => console.log(number));
-console.log(numbers.includes(0));
-
-console.log(numbers.indexOf(23));
+const newNumbers = numbers.forEach(number => number * 2);
+console.log(newNumbers);
+const mapNumbers = numbers.map(number => number * 2);
+console.log(mapNumbers);
