983ad06a50cd95cf78bdee3d664f3ceb03a39a19
mprzybysz <notentego678@gmail.com>
2022-04-18 14:04:47 +0200

    arrow function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
-const heading = document.querySelector('h1');
+const arrowFunction = () => {};
 
+const test = name => {
+	console.log(`Mam na imię ${name}.`);
+};
 
+test('Mateusz');
 
-heading.addEventListener('click', function () {
-	console.log('Click header');
-});
+const test3 = name => console.log(`Mam na imię ${name}.`);
+
+test3('Mateusz');
+
+function add(num1, num2) {
+	return num1 + num2;
+}
+
+const add2 = (num1, num2) => num1 + num2; //return
+console.log(add2(5, 5));
+
