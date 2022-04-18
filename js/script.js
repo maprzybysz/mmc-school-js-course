ee91b144822c7d775712d7e3681aaa66768e4e3e
mprzybysz <notentego678@gmail.com>
2022-04-18 15:09:53 +0200

    fetch elements on page 2

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-const liById = document.getElementById('item');
-console.log(liById);
+// const test = document.querySelector('ul li');
+// console.log(test);
 
-const ulByTagname = document.getElementsByTagName('li');
-console.log(ulByTagname);
+const test2 = document.getElementsByTagName('li');
 
-const elementByClassName = document.getElementsByClassName('test');
-console.log(elementByClassName);
+const ulList = document.querySelector('ul');
+console.log(ulList);
+
+const liItem = ulList.querySelector('li');
+console.log(liItem);
+
+const liItems = document.querySelectorAll('li');
+console.log(liItems);
+
+const newLi = document.createElement('li');
+ulList.appendChild(newLi).textContent = 'new';
+
+console.log(liItems.length);
+console.log(test2.length);
