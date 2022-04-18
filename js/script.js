802f7934c7ba8d5f3b77d3b5f7605473cbbd61eb
mprzybysz <notentego678@gmail.com>
2022-04-18 10:12:32 +0200

    task

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
-const msg = 'testowa wiadomość';
+const text1 = 'powiększ mnie!';
+const text2 = 'ZAPISZ MNIE MAŁYMI LITERAMI';
+const text3 = '$#%#^ wytnij te dziwne znaki na początku!';
+const text4 = 'sprawdź, czy zawieram słowo "czy"';
+const text5 = 'wyLoguj w konsoli tylko literę "L", która znajduje się w wyrazie "Wyloguj"';
+const text6 = 'pies zamień każde słowo pies, na słowo kot pies';
+const text7 = 'podziel, ten, string, od, przecinków';
 
-console.log(msg.length);
-
-const newMsg = msg.toUpperCase();
-
-console.log(newMsg);
-
-const username = 'mefiu';
-
-const newUsername = username.charAt(0).toUpperCase() + username.slice(1);
-
-console.log(newUsername);
+console.log('text1');
+console.log(text1.toUpperCase());
+console.log('text2');
+console.log(text2.toLowerCase());
+console.log('text3');
+console.log(text3.slice(6));
+console.log('text4');
+console.log(text4.includes('czy'));
+console.log('text5');
+console.log(text5.charAt(2));
+console.log('text6');
+console.log(text6.replaceAll('pies', 'kot'));
+console.log('text7');
+console.log(text7.split(','));
