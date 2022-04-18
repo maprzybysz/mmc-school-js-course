b5b9fbd43ca9eaffcb86205971d983a975cc88b3
mprzybysz <notentego678@gmail.com>
2022-04-18 12:35:34 +0200

    task 4

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-let x = 20;
+const numbers = [5, 8, 10, 23, 48, 60];
 
-do {
-	x -= 3;
-} while (x > 0);
+for (const number of numbers) {
+	console.log(number / 5);
+}
 
-console.log(x);
+for (const number of numbers) {
+	if (number % 2 === 0) {
+		console.log(`Liczba ${number} jest parzysta`);
+	} else {
+		console.log(`Liczba ${number} nie jest parzysta`);
+	}
+}
