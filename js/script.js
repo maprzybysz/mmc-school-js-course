53bba1ea32741edb9bce500bb54fcc84ded4be3b
mprzybysz <notentego678@gmail.com>
2022-04-18 14:09:32 +0200

    default function parameters

--- js/script.js
+++ js/script.js
@@ -1,19 +1,6 @@
-const arrowFunction = () => {};
-
-const test = name => {
-	console.log(`Mam na imię ${name}.`);
+const hello = (name = 'drogi użytkowniku') => {
+	console.log(`Cześć ${name}, jak się masz?`);
 };
 
-test('Mateusz');
-
-const test3 = name => console.log(`Mam na imię ${name}.`);
-
-test3('Mateusz');
-
-function add(num1, num2) {
-	return num1 + num2;
-}
-
-const add2 = (num1, num2) => num1 + num2; //return
-console.log(add2(5, 5));
-
+hello();
+hello('Mateusz');
