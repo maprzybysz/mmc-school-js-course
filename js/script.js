a65e5ea60fa8e7c26f6eba381a0f309bffdfadd5
mprzybysz <notentego678@gmail.com>
2022-04-19 16:56:26 +0200

    addEventListener

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-const div = document.querySelector('div');
-const p = document.querySelector('p');
+const btn1 = document.querySelector('.btn-1');
+const btn2 = document.querySelector('.btn-2');
+const btn3 = document.querySelector('.btn-3');
 
-// div.removeChild(p);
+// btn1.addEventListener('click', () => {
+// 	console.log('click');
+// });
 
-p.remove();
+// btn2.addEventListener('mouseover', () => {
+// 	console.log('mouseover');
+// });
+
+// btn3.addEventListener('dblclick', () => {
+// 	console.log('double click');
+// });
+
+const btns = document.querySelectorAll('button');
+console.log(btns);
+
+const smile = () => {
+	console.log(':)');
+};
+
+btns.forEach(btn => btn.addEventListener('click', smile));
