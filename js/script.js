05ba55a05e54f815c11078f893ca273d846ab2cf
mprzybysz <notentego678@gmail.com>
2022-04-19 21:56:05 +0200

    setTimeout, setInterval

--- js/script.js
+++ js/script.js
@@ -1,29 +1,7 @@
-const div = document.querySelector('.square');
-const btn1 = document.querySelector('.btn1');
-const btn2 = document.querySelector('.btn2');
-const p1 = document.querySelector('.p1');
-const p2 = document.querySelector('.p2');
-
-const helloFn = () => {
-	console.log('cześć');
+const test = () => {
+	console.log('hello');
 };
 
-const blueFn = () => {
-	div.style.backgroundColor = 'blue';
-};
+// setTimeout(test, 1000);
 
-const redFn = () => {
-	div.style.backgroundColor = 'red';
-};
-
-const toggleFn = () => {
-	p1.classList.toggle('show');
-	p2.classList.toggle('show');
-};
-
-btn1.addEventListener('dblclick', helloFn);
-
-div.addEventListener('mouseover', redFn);
-div.addEventListener('mouseout', blueFn);
-
-btn2.addEventListener('click', toggleFn);
+setInterval(test, 1000);
