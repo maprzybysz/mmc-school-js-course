401a63453bb14b8e868835f6b1f329da83856762
mprzybysz <notentego678@gmail.com>
2022-04-19 22:13:28 +0200

    data attributes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,2 @@
-const link = document.querySelector('a');
-const img = document.querySelector('img');
-const p = document.querySelector('p');
-
-link.setAttribute('href', '#');
-link.setAttribute('target', '_blank');
-
-console.log(img.getAttribute('alt'));
-
-console.log(p.hasAttribute('style'));
-
-p.removeAttribute('style');
+const div = document.querySelector('[data-number="23"]');
+console.log(div.dataset.moreInfo);
