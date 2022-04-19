dbe1aa2ebdc78741964df6af87d1e28496fbc139
mprzybysz <notentego678@gmail.com>
2022-04-19 22:18:06 +0200

    relationships

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
-const div = document.querySelector('[data-number="23"]');
-console.log(div.dataset.moreInfo);
+const grandpa = document.querySelector('.grandparent');
+// console.log(grandpa.querySelector('.parent'));
+
+const middleChild = document.querySelector('.middle-child');
+
+const prevSibling = middleChild.previousElementSibling;
+
+const nextSibling = middleChild.nextElementSibling;
+
+const parentEl = middleChild.parentElement;
