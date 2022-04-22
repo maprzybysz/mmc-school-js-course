378533c383811b8af01705180848b228014f5377
mprzybysz <notentego678@gmail.com>
2022-04-22 17:07:00 +0200

    project 2

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
-const arrowBtn = document.querySelector('.arrow');
-const arrowIcon = document.querySelector('.fas');
-const img = document.querySelector('.item1');
+const sizeUp = document.querySelector('.sizeUp');
+const sizeDown = document.querySelector('.sizeDown');
+const colorBtn = document.querySelector('.color');
+const p = document.querySelector('p');
 
-const showImg = () => {
-	img.classList.toggle('hide');
+let fontSize = 36;
 
-	if (img.classList.contains('hide')) {
-		arrowIcon.style.transform = 'rotate(-180deg)';
-	} else {
-		arrowIcon.style.transform = 'rotate(0)';
-	}
+const increase = () => {
+	fontSize += 5;
+	p.style.fontSize = fontSize + 'px';
 };
 
-arrowBtn.addEventListener('click', showImg);
+const decrease = () => {
+	if (fontSize <= 21) return;
+
+	fontSize -= 5;
+	p.style.fontSize = fontSize + 'px';
+};
+
+const colorChanger = () => {
+	const r = Math.floor(Math.random() * 255);
+	const g = Math.floor(Math.random() * 255);
+	const b = Math.floor(Math.random() * 255);
+	p.style.color = `rgb(${r},${g},${b})`;
+};
+
+sizeUp.addEventListener('click', increase);
+sizeDown.addEventListener('click', decrease);
+colorBtn.addEventListener('click', colorChanger);
