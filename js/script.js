4f12244edaf88e576883e884d2f6bf6b94e2bec5
mprzybysz <notentego678@gmail.com>
2022-04-23 22:43:40 +0200

    destructuring

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
-const user = {
+const person = {
 	name: 'Mateusz',
-	age: 23
+	age: 23,
+	job: 'Programmer',
+	car: {
+		brand: 'Mazda',
+		model: '3'
+	}
 };
 
-const pet = {
-	name: 'Drops',
-	type: 'dog'
+const showInfo = ({ name, job, age }) => {
+	// const { name: firstName, job, age } = person;
+	// const { name, job, age } = person;
+	// console.log(`${person.name} pracuje jako ${person.job} i ma ${person.age} lata.`);
+	console.log(`${name} pracuje jako ${job} i ma ${age} lata.`);
+	// console.log(`${firstName} pracuje jako ${job} i ma ${age} lata.`);
 };
 
-const userInfo = Object.assign({}, user, pet);
-console.log(user);
-console.log(pet);
-console.log(userInfo);
+showInfo(person);
 
-const userCopy = Object.assign({}, user);
-userCopy.age = 25;
-console.log(userCopy);
-console.log(user);
+const showInfo2 = ({ car: { brand, model } }) => {
+	// console.log(car);
+	console.log(`Mój samochód to ${brand} ${model}.`);
+};
+
+const showInfo3 = () => {
+	const {
+		car: { brand, model }
+	} = person;
+	console.log(`Mój samochód to ${brand} ${model}.`);
+};
+
+showInfo2(person);
+showInfo3(person);
