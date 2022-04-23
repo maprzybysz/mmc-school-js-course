7aa183b60bd6258234a5c23620481000b13086af
mprzybysz <notentego678@gmail.com>
2022-04-23 21:57:47 +0200

    bind

--- js/script.js
+++ js/script.js
@@ -1,30 +1,18 @@
-const btns = document.querySelectorAll('button');
+function test() {
+	console.log(this);
+	console.log(this.name);
+}
 
-btns.forEach(btn =>
-	// btn.addEventListener('click', function () {
-	// 	console.log(this);
-	// })
-	// btn.addEventListener('click', function (e) {
-	// 	console.log(e.target);
-	// })
-	btn.addEventListener('click', e => {
-		console.log(this.textContent);
-		console.log(e.target.textContent);
-	})
-);
-
-const ob = {
-	number: 123,
-	showNumber() {
-		console.log(this.number);
-	},
-	showNumber2() {
-		const test = () => {
-			console.log(this.number);
-		};
-		test();
-	}
+const car1 = {
+	name: 'Audi'
 };
 
-ob.showNumber();
-ob.showNumber2;
+const car2 = {
+	name: 'Doge'
+};
+
+const car3 = {
+	name: 'BMW'
+};
+
+test.bind(car1)();
