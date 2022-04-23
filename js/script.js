273d68126215509ebcb2abc01c0e93aaac395bd5
mprzybysz <notentego678@gmail.com>
2022-04-23 21:42:22 +0200

    task

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
-function User(name, age) {
+const btn1 = document.querySelector('.btn-1');
+const btn2 = document.querySelector('.btn-2');
+const btn3 = document.querySelector('.btn-3');
+
+function Meal(name, price) {
 	this.name = name;
-	this.age = age;
+	this.price = price;
 }
 
-// class User {
-// 	constructor(name, age) {
-// 		this.name = name;
-// 		this.age = age;
-// 	}
-// }
+const meal1 = new Meal('meal1', 100);
+const meal2 = new Meal('meal2', 75);
+const meal3 = new Meal('meal3', 50);
 
-const newUser = new User('Mateusz', 23);
-const newUser2 = new User('Tomasz', 34);
-
-console.log(newUser);
-console.log(newUser2);
-User.prototype.country = 'Polska';
-User.prototype.hello = function () {
-	console.log(`Cześć ${this.name}!`);
+Meal.prototype.fn = function () {
+	console.log(`${this.name} kosztuje ${this.price}`);
 };
-console.log(newUser.country);
-newUser.hello();
+
+btn1.addEventListener('click', () => {
+	meal1.fn();
+});
+btn2.addEventListener('click', () => {
+	meal2.fn();
+});
+btn3.addEventListener('click', () => {
+	meal3.fn();
+});
