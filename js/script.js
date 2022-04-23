2486bb8a24262465dc0805d7422402a8160089c5
mprzybysz <notentego678@gmail.com>
2022-04-23 22:46:09 +0200

    destructuring of arrays

--- js/script.js
+++ js/script.js
@@ -1,34 +1,9 @@
-const person = {
-	name: 'Mateusz',
-	age: 23,
-	job: 'Programmer',
-	car: {
-		brand: 'Mazda',
-		model: '3'
-	}
-};
+const colors = ['red', 'green', 'blue'];
 
-const showInfo = ({ name, job, age }) => {
-	// const { name: firstName, job, age } = person;
-	// const { name, job, age } = person;
-	// console.log(`${person.name} pracuje jako ${person.job} i ma ${person.age} lata.`);
-	console.log(`${name} pracuje jako ${job} i ma ${age} lata.`);
-	// console.log(`${firstName} pracuje jako ${job} i ma ${age} lata.`);
-};
+const firstColor = colors[0];
+const secondColor = colors[1];
 
-showInfo(person);
+const [first, second] = colors;
 
-const showInfo2 = ({ car: { brand, model } }) => {
-	// console.log(car);
-	console.log(`Mój samochód to ${brand} ${model}.`);
-};
-
-const showInfo3 = () => {
-	const {
-		car: { brand, model }
-	} = person;
-	console.log(`Mój samochód to ${brand} ${model}.`);
-};
-
-showInfo2(person);
-showInfo3(person);
+console.log(firstColor, secondColor);
+console.log(first, second);
