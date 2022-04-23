88483706652d3e9d0fca2dcdcfdb47490e729e21
mprzybysz <notentego678@gmail.com>
2022-04-23 21:49:06 +0200

    this part1

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
-const btn1 = document.querySelector('.btn-1');
-const btn2 = document.querySelector('.btn-2');
-const btn3 = document.querySelector('.btn-3');
+// console.log(this);
 
-function Meal(name, price) {
+// function test() {
+// 	console.log(this);
+// }
+
+// test();
+
+const person = {
+	name: 'Mefiu',
+	// showInfo() {
+	// 	console.log(this);
+	// 	console.log(this.name);
+	// }
+	address: {
+		city: 'Kraków',
+		showCity: function () {
+			console.log(this);
+		}
+	}
+};
+person.address.showCity();
+
+function Food(name) {
 	this.name = name;
-	this.price = price;
 }
 
-const meal1 = new Meal('meal1', 100);
-const meal2 = new Meal('meal2', 75);
-const meal3 = new Meal('meal3', 50);
+const meal1 = new Food('schabowy');
+const meal2 = new Food('pepsi');
 
-Meal.prototype.fn = function () {
-	console.log(`${this.name} kosztuje ${this.price}`);
-};
-
-btn1.addEventListener('click', () => {
-	meal1.fn();
-});
-btn2.addEventListener('click', () => {
-	meal2.fn();
-});
-btn3.addEventListener('click', () => {
-	meal3.fn();
-});
+console.log(meal1, meal2);
