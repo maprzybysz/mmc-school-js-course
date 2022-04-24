c82cc4e6f5a70d317aaced671bc2661f4529f578
mprzybysz <notentego678@gmail.com>
2022-04-24 21:39:32 +0200

    project 8

--- js/script.js
+++ js/script.js
@@ -1,14 +1,61 @@
-const one = document.querySelector('.one');
-const two = document.querySelector('.two');
-const three = document.querySelector('.three');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const cityName = document.querySelector('.city-name');
+const warning = document.querySelector('.warning');
+const photo = document.querySelector('.photo');
+const weather = document.querySelector('.weather');
+const temperature = document.querySelector('.temperature');
+const humidity = document.querySelector('.humidity');
 
-const URL = 'https://dog.ceo/api/breeds/image/random';
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const API_KEY = '&appid=5c1ec568cbce47a06dc93569efd1e095';
+const API_UNITS = '&units=metric';
 
-axios.get(URL).then(res => two.setAttribute('src', res.data.message));
+const getWeather = () => {
+	const city = input.value || 'Warsaw';
+	const URL = API_LINK + city + API_KEY + API_UNITS;
 
-async function showImage() {
-	const res = await axios.get(URL);
-	three.setAttribute('src', res.data.message);
-}
+	axios
+		.get(URL)
+		.then(res => {
+			const temp = res.data.main.temp;
+			const hum = res.data.main.humidity;
+			const status = Object.assign({}, ...res.data.weather);
 
-showImage();
+			weather.textContent = status.main;
+			cityName.textContent = res.data.name;
+			temperature.textContent = Math.floor(temp) + '°C';
+			humidity.textContent = hum + '%';
+
+			warning.textContent = '';
+			input.value = '';
+			if (status.id >= 200 && status.id < 300) {
+				photo.setAttribute('src', './img/thunderstorm.png');
+			} else if (status.id >= 300 && status.id < 400) {
+				photo.setAttribute('src', './img/drizzle.png');
+			} else if (status.id >= 500 && status.id < 600) {
+				photo.setAttribute('src', './img/rain.png');
+			} else if (status.id >= 600 && status.id < 700) {
+				photo.setAttribute('src', './img/ice.png');
+			} else if (status.id >= 700 && status.id < 800) {
+				photo.setAttribute('src', './img/fog.png');
+			} else if (status.id === 800) {
+				photo.setAttribute('src', './img/sun.png');
+			} else if (status.id > 800 && status.id < 900) {
+				photo.setAttribute('src', './img/cloud.png');
+			} else {
+				photo.setAttribute('src', './img/unknown.png');
+			}
+		})
+		.catch(() => (warning.textContent = 'Wpisz poprawną nazwę miasta'));
+};
+
+const enterCheck = e => {
+	if (e.key === 'Enter') {
+		getWeather();
+	}
+};
+
+getWeather();
+button.addEventListener('click', getWeather);
+input.addEventListener('keyup', enterCheck);
