0f93b37a5f0a9762c81dfceaa1189f5324c490d6
mprzybysz <notentego678@gmail.com>
2022-04-24 18:31:05 +0200

    async await part 1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,39 @@
-const url = 'fajnastrona.pl';
-
-const test = {
-	method: 'POST'
+const checkAge = age => {
+	return new Promise((resolve, reject) => {
+		if (age >= 18) {
+			resolve();
+		} else {
+			reject();
+		}
+	});
 };
 
-fetch(url, {
-	method: 'GET'
-});
+const doubleCheck = () => {
+	return new Promise((resolve, reject) => {
+		console.log('Sprawdzam jeszcze raz...');
+		setTimeout(() => {
+			resolve('Faktycznie, wiek się zgadza.');
+		}, 1000);
+	});
+};
 
-fetch(url, test);
+// checkAge(18)
+// 	.then(() => {
+// 		console.log('Chyba możesz wejść');
+// 		return doubleCheck();
+// 	})
+// 	.then(res => console.log(res))
+// 	.then(() => console.log('Weryfikacja zakończona'))
+// 	.catch(err => console.error(err));
+
+async function test() {
+	try {
+		await checkAge(2);
+		console.log('Chyba możesz wejść');
+		await doubleCheck();
+	} catch {
+		console.error('Masz za mało lat!');
+	}
+}
+
+test();
