2b06def117838271bbaff8f79d2aa82737603129
mprzybysz <notentego678@gmail.com>
2022-04-24 12:40:26 +0200

    asynchronous vs synchronous

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
-const user = {
-	name: 'Mateusz',
-	age: 24
-};
+const btn = document.querySelector('button');
 
-// const obj = {
-// 	"name": "Mateusz",
-// 	"age": 23
-// };
+btn.addEventListener('click', () => {
+	console.log('wykonuje się pierwszy!');
+	const p = document.createElement('p');
+	alert('stworzyłem paragraf!');
+	p.textContent = 'test';
+	document.body.append(p);
+	console.log('koniec');
+});
+
+console.log(1);
+setTimeout(() => {
+	console.log(2);
+}, 800);
+console.log(3);
