2701e9d471159c97ce056f6d43a54cf012598383
mprzybysz <notentego678@gmail.com>
2022-04-24 18:35:06 +0200

    async await part 2

--- js/script.js
+++ js/script.js
@@ -1,38 +1,18 @@
-const checkAge = age => {
-	return new Promise((resolve, reject) => {
-		if (age >= 18) {
-			resolve();
-		} else {
-			reject();
-		}
-	});
-};
+const URL = 'https://dog.ceo/api/breeds/image/random';
 
-const doubleCheck = () => {
-	return new Promise((resolve, reject) => {
-		console.log('Sprawdzam jeszcze raz...');
-		setTimeout(() => {
-			resolve('Faktycznie, wiek się zgadza.');
-		}, 1000);
-	});
-};
-
-// checkAge(18)
-// 	.then(() => {
-// 		console.log('Chyba możesz wejść');
-// 		return doubleCheck();
-// 	})
-// 	.then(res => console.log(res))
-// 	.then(() => console.log('Weryfikacja zakończona'))
-// 	.catch(err => console.error(err));
+fetch(URL)
+	.then(res => res.json())
+	.then(data => console.log(data))
+	.catch(error => console.error(error));
 
 async function test() {
 	try {
-		await checkAge(2);
-		console.log('Chyba możesz wejść');
-		await doubleCheck();
+		const res = await fetch(URL);
+		const data = await res.json();
+
+		console.log(data);
 	} catch {
-		console.error('Masz za mało lat!');
+		console.error(error);
 	}
 }
 
