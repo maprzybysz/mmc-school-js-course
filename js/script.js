66c03e0e12ef801226dacde16d648e7d941b7cfd
mprzybysz <notentego678@gmail.com>
2022-04-24 12:52:37 +0200

    http methods

--- js/script.js
+++ js/script.js
@@ -1,38 +1,11 @@
-// setTimeout(() => {
-// 	console.log(1);
-// 	setTimeout(() => {
-// 		console.log(2);
-// 		setTimeout(() => {
-// 			console.log(3);
-// 			setTimeout(() => {
-// 				console.log(4);
-// 				setTimeout(() => {
-// 					console.log(5);
-// 				}, 300);
-// 			}, 300);
-// 		}, 300);
-// 	}, 300);
-// }, 300);
+const url = 'fajnastrona.pl';
 
-// const newPromise = () => {};
+const test = {
+	method: 'POST'
+};
 
-// newPromise()
-// 	.then(() => {
-// 		console.log('1');
-// 		return newPromise();
-// 	})
-// 	.then(() => {
-// 		console.log('1');
-// 		return newPromise();
-// 	})
-// 	.catch(err => console.log(err));
-
-const test = new Promise((resolve, reject) => {
-	if (true) {
-		resolve('jest ok');
-	} else {
-		reject('blad');
-	}
+fetch(url, {
+	method: 'GET'
 });
 
-test.then(info => console.log(info)).catch(err => console.log(err));
+fetch(url, test);
